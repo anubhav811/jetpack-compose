e845c0f082cb5ae4167f6673064bceaba75f92d2
Anubhav Chachra <chachraanubhav@gmail.com>
2022-02-01 01:31:27 +0530

    height,width,padding,offset

--- modifiers/app/src/main/java/com/anubhav/modifiers/MainActivity.kt
+++ modifiers/app/src/main/java/com/anubhav/modifiers/MainActivity.kt
@@ -3,20 +3,31 @@ package com.anubhav.modifiers
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import com.anubhav.modifiers.ui.theme.ModifiersTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+        // Modifiers are basically used to modify properties of any composable
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .background(Color.Green)
+                    .fillMaxWidth()
+                    .padding(60.dp)
+            ){
+                Text("Hello",modifier=Modifier.offset(50.dp))
+                Text("World")
+            }
         }
     }
 }
